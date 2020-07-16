2a974d280ed795d31dc9f2216f4be40d0cd1fd43
aniket-hue <freakyyo26@gmail.com>
2020-07-16 08:20:41 +0530

    First Commit

---
+++ src/Root.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class Root extends Component {
+    render() {
+        return (
+            <div>
+                Hello
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <Root />,
+    document.getElementById('root')
+)
